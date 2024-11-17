567465df59ba6915cc8a3a1471b68aab59a7d7df
fiflorek <filip.florek@protonmail.com>
2024-11-17 13:54:01 -0700

    make result a dataclass

--- src/algorithm/result.py
+++ src/algorithm/result.py
@@ -1,12 +1,12 @@
+from dataclasses import dataclass
 from typing import List
 
 
+@dataclass
 class Result:
-
-    def __init__(self, best: float, average: float, best_genotype: List[int]):
-        self.best = best
-        self.average = average
-        self.best_genotype = best_genotype
+    best: float
+    average: float
+    best_genotype: List[int]
 
     def __str__(self):
         return f"Best: {self.best}, Average: {self.average}, Best genotype: {self.best_genotype}"
