0732190752be745d97e123a2d5704492091f77d7
fiflorek <filip.florek@protonmail.com>
2024-11-20 20:11:56 -0700

    refactor tabu search

--- src/algorithm/tabu_search.py
+++ src/algorithm/tabu_search.py
@@ -47,12 +47,7 @@ class TabuSearch(Algorithm):
             # if all neighbours were already visited (are in tabu list) we're switching the neighbourhood drastically
             # (random genotype)
             if best_neighbour is None:
-                genotype = list(range(1, self.no_of_cities))
-                random.shuffle(genotype)
-                while hash(tuple(genotype)) in self.tabu_set:
-                    random.shuffle(genotype)
-                best_neighbour = Individual(genotype)
-                best_neighbour.evaluate(self.cvrp)
+                best_neighbour = self.change_neighbourhood()
             self.current_best = best_neighbour
             self.update_tabu(best_neighbour)
             avg_fitness = round(sum([neighbour.fitness for neighbour in neighbours]) / len(neighbours), 2)
@@ -72,9 +67,6 @@ class TabuSearch(Algorithm):
     def search_neighbourhood(self) -> list[Individual]:
         neighbours: list[Individual] = []
         for i in range(self.neighbourhood_size):
-            # TO CHECK: is it updated in place?
-            # neighbour_genotype = self.current_best.genotype[:]
-            # individual.mutate(neighbour_genotype, self.mutation_type)
             neighbour_genotype = individual.mutate(self.current_best.genotype[:], self.mutation_type)
             neighbour = Individual(neighbour_genotype)
             neighbour.evaluate(self.cvrp)
@@ -97,3 +89,12 @@ class TabuSearch(Algorithm):
         self.tabu_list.append(genotype_hash)
         if len(self.tabu_list) > self.config.tabu_list_size:
             self.tabu_set.remove(self.tabu_list.pop(0))
+
+    def change_neighbourhood(self) -> Individual:
+        genotype = list(range(1, self.no_of_cities))
+        random.shuffle(genotype)
+        while hash(tuple(genotype)) in self.tabu_set:
+            random.shuffle(genotype)
+        i = Individual(genotype)
+        i.evaluate(self.cvrp)
+        return i
